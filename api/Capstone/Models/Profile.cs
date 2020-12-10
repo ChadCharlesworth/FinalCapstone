f0be7467989b5cc4fa70ac0e6d4c18a482d52c16
Dan Eisner <deisner@g.ucla.edu>
2020-12-10 09:56:13 -0500

    profile model completed

--- api/Capstone/Models/Profile.cs
+++ api/Capstone/Models/Profile.cs
@@ -7,5 +7,13 @@ namespace Capstone.Models
 {
     public class Profile
     {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string user_role { get; set; }
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public List<int> Address_Ids { get; set; } = new List<int>();
+        public List<int> Pet_Ids { get; set; } = new List<int>();
+        public List<Playdate> Playdates { get; set; } = new List<Playdate>();
     }
 }
