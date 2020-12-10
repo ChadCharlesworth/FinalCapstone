834f1fdadf7ededebcd397238b33c7ac5c316e18
Dan Eisner <deisner@g.ucla.edu>
2020-12-10 09:58:37 -0500

    added [Required] to some Profile properties

--- api/Capstone/Models/Profile.cs
+++ api/Capstone/Models/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,24 @@ namespace Capstone.Models
 {
     public class Profile
     {
+        [Required]
         public string First_Name { get; set; }
+
+        [Required]
         public string Last_Name { get; set; }
+        
         public string user_role { get; set; }
+        
+        [Required]
         public int user_id { get; set; }
+        
+        [Required]
         public string username { get; set; }
+        
         public List<int> Address_Ids { get; set; } = new List<int>();
+        
         public List<int> Pet_Ids { get; set; } = new List<int>();
+        
         public List<Playdate> Playdates { get; set; } = new List<Playdate>();
     }
 }
