e8277335afd4e49b4c71ebf3f0ac67ab58e5ecb4
Chad Charlesworth <chadwulf516@gmail.com>
2020-12-12 10:23:26 -0500

    Starting Bootstrap changes to register and app.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    <h1 class="display-1 text-center"> Pet Play Pals </h1>
+    <div id="nav" class="navbar bg-info navbar-light sticky-top">
+      <router-link class="nav-item nav-link" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link class="nav-item nav-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      
     </div>
-    <router-view />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-2 left-column border border-dark" style="height:100vh;"> PLACEHOLDER </div>
+        <div class="col-8 main"><router-view /></div>
+        <div class="col-2 right-column border border-dark">PLACEHOLDER</div>
+      </div>
+    </div>
   </div>
 </template>
