4057016e46d98c38c7c33ed231d4de1b267c7bc1
Dan Eisner <deisner@g.ucla.edu>
2020-12-13 11:28:52 -0500

    services written, store populated

---
+++ vue/src/services/PlaydateService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default {
+
+  getPlaydates() {
+    return axios.get('api/playdate');
+  },
+
+  addPlaydate(playdate) {
+    return axios.post('api/playdate', playdate)
+  },
+
+  updatePlaydate(playdateID, playdate) {
+    return axios.put(`api/playdate/${playdateID}`, playdate)
+  },
+
+  updatePlaydateByPetID(playdateID, playdate, petID) {
+    return axios.put(`api/playdate/${playdateID}/pet/${petID}`, playdate)
+  },
+
+  deletePlaydate(playdateID) {
+    return axios.post(`api/playdate/${playdateID}`)
+  }
+
+}
