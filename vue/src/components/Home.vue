4b473deaeecbd1bedb71d600d353e7c56698b2c2
Angela Zollman <amnagem@gmail.com>
2020-12-13 17:43:08 -0500

    idk what I'm doing

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -16,9 +16,29 @@
 <script>
 export default {
   name: "home",
+//   computed: {publicPlaydates() {
+//   return this.$store.state.playdates.filter(playdate => {
+//     return playdate.is_Private == false;
+//   })
+// },
+// playdateLocation(playdate) {
+//   let location = this.$store.state.addresses.filter(address => {
+//     return address.address_ID == playdate.address_ID;
+//   });
+//   return location;
+// },
+// displayPlaydates() {
+//   let display = [];
+//   this.publicPlaydates.forEach(playdate => {
+// display.push( {location: this.playdateLocation(playdate)[0].street_Address_1, date_and_time: playdate.date_Time} )
+//   }) 
+//   return display;
+
   data() {
       return {
+        fields: ["location", "date_and_time"],
         items: [
+          // this.displayPlaydates
           { location: "Frick Park", date_and_time: "01/01/21" },
           { location: "Schenley Park", date_and_time: "01/02/21" },
           { location: "Point State Park", date_and_time: "01/03/21" },
