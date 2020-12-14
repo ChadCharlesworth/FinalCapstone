e17c6e22cf44e54281a953a5db0237674e0e5f60
Dan Eisner <deisner@g.ucla.edu>
2020-12-14 09:58:57 -0500

    adding playdate info to profile

---
+++ vue/src/components/PlaydateInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      {{playdate.date_Time}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "playdate-info",
+  props: { playdate: Object },
+
+}
+</script>
+
+<style>
+
+</style>
