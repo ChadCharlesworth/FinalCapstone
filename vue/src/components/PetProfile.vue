188989af56da16fed0292b6faa62f73fd9abb536
Dan Eisner <deisner@g.ucla.edu>
2020-12-14 01:26:57 -0500

    displaying pets

--- vue/src/components/PetProfile.vue
+++ vue/src/components/PetProfile.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        <h1>{{pet.pet_Name}}</h1>
-        </div>
+  <div>
+    <h1>{{ pet.pet_Name }}</h1>
+    <h3>{{ pet.species }}</h3>
+    <h4>Breed: {{ pet.breed }}</h4>
+    <h4>Personality: {{ pet.personality }}</h4>
+    <h4>Size: {{pet.size}}</h4>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "pet-profile",
-    props: ["pet"]
-    
-}
+  name: "pet-profile",
+  props: { pet: Object },
+};
 </script>
