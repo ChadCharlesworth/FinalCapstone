bebab2ed7306e0234737c4b9453fc4073e935284
Dan Eisner <deisner@g.ucla.edu>
2020-12-14 11:56:35 -0500

    adding computations for addresses and dates

--- vue/src/components/PlaydateInfo.vue
+++ vue/src/components/PlaydateInfo.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-      {{playdate.date_Time}}
+    <h3>{{ playdateDate }} {{playdateTime}}</h3>
+    <p>
+      {{ playdateAddress.street_Address_1 }}
+      {{ playdateAddress.street_Address_2 }}<br />
+      {{ playdateAddress.city }}, {{ playdateAddress.state }}
+      {{ playdateAddress.zip }}
+    </p>
   </div>
 </template>
 
@@ -8,10 +14,32 @@
 export default {
   name: "playdate-info",
   props: { playdate: Object },
-
-}
+  computed: {
+    playdateAddress() {
+      let playdateAddress;
+      let tempPlaydateAddress = [];
+      const playdate = this.playdate;
+      const addresses = this.$store.state.addresses;
+      tempPlaydateAddress = addresses.filter((address) => {
+        return address.address_ID === playdate.address_ID;
+      });
+      playdateAddress = tempPlaydateAddress[0];
+      return playdateAddress;
+    },
+    playdateDate() {
+        return this.playdate.date_Time.split("T")[0];
+    },
+    playdateTime() {
+        let time = this.playdate.date_Time.split("T")[1];
+        time = time.substring(0,5);
+        if(time.startsWith('0')) {
+            time = time.substring(1);
+        }
+        return time;
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
